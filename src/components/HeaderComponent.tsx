edf866820d6e5b30501e21a7ec9852094189127b
brandoughnut <brandonnguyen2867@gmail.com>
2024-03-19 13:42:07 -0700

    got light and dark mode functionality

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../App.css";
 import TopRowCardsComponent from "./TopRowCardsComponent";
 import facebook from "../images/icon-facebook.svg";
@@ -10,23 +10,58 @@ import down from "../images/icon-down.svg";
 import BottomRowCardsComponent from "./BottomRowCardsComponent";
 
 const HeaderComponent = () => {
+
+  const [toggle, setToggle] = useState<boolean>(false);
+  const [BGTop, setBGTop] = useState<string>('darkBGTop');
+  const [BG, setBG] = useState<string>('darkBG');
+  const [headText, setHeadText] = useState<string>('darkHeadText');
+  const [text2, setText2] = useState<string>('darkText2');
+  const [toggleBtn, setToggleBtn] = useState<string>('darkToggleBtn');
+  const [card, setCard] = useState<string>('darkCard');
+
+  useEffect(()=> {
+
+    if(toggle === false){
+      setBGTop('darkBGTop');
+      setBG('darkBG');
+      setHeadText('darkHeadText');
+      setText2('darkText2');
+      setToggleBtn('darkToggleBtn');
+      setCard('darkCard');
+    }else{
+      setBGTop('lightBGTop');
+      setBG('lightBG');
+      setHeadText('lightHeadText');
+      setText2('lightText2');
+      setToggleBtn('lightToggleBtn');
+      setCard('lightCard');
+    }
+
+  }, [toggle])
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  }
+
+
+
   return (
     <div>
-      <div className="darkBG">
-        <div className="darkBGTop">
+      <div className={BG}>
+        <div className={BGTop}>
           <div className="grid grid-cols-2">
             <div>
-              <h1 className=" darkHeadText text-[27px] leading-7">
+              <h1 className={`${headText} text-[27px] leading-7`}>
                 Social Media Dashboard
               </h1>
-              <p className="darkText2">Total Followers: 23,064</p>
+              <p className={text2}>Total Followers: 23,064</p>
             </div>
             <div className="grid justify-end my-auto">
-              <div className="darkToggleBtn">
+              <div className={toggleBtn}>
                 <span className="me-3 inline">Dark Mode</span>
                 <label className="switch">
                   <input type="checkbox" />
-                  <span className="slider"></span>
+                  <span className="slider" onClick={handleToggle}></span>
                 </label>
               </div>
             </div>
@@ -43,6 +78,7 @@ const HeaderComponent = () => {
                 logo={facebook}
                 username="@nathanf"
                 topBorder="facebook"
+                text={text2} headText={headText} card={card}
               />
             </div>
             <div className="mx-5">
@@ -55,6 +91,7 @@ const HeaderComponent = () => {
                 logo={twitter}
                 username="@nathanf"
                 topBorder="twitter"
+                text={text2} headText={headText} card={card}
               />
             </div>
             <div className="mx-5">
@@ -67,6 +104,7 @@ const HeaderComponent = () => {
                 logo={instagram}
                 username="@realnathanf"
                 topBorder="instagram"
+                text={text2} headText={headText} card={card}
               />
             </div>
             <div className="mx-5">
@@ -79,38 +117,39 @@ const HeaderComponent = () => {
                 logo={youtube}
                 username="Nathan F."
                 topBorder="youtube"
+                text={text2} headText={headText} card={card}
               />
             </div>
           </div>
 
           <div>
-            <h1 className="darkHeadText text-[24px]">Overview - Today</h1>
+            <h1 className={`${headText} text-[24px]`}>Overview - Today</h1>
           </div>
 
           <div className="grid grid-cols-4">
             <div className="mx-5">
-                <BottomRowCardsComponent statsColor='statsTextUp' statLogo={up} stats='3%' logo={facebook} followers='87' type='Page Views'/>
+                <BottomRowCardsComponent statsColor='statsTextUp' statLogo={up} stats='3%' logo={facebook} followers='87' type='Page Views' text={text2} headText={headText} card={card}/>
             </div>
             <div className="mx-5">
-                <BottomRowCardsComponent statsColor='statsTextDown' statLogo={down} stats='2%' logo={facebook} followers='52' type='Likes'/>
+                <BottomRowCardsComponent statsColor='statsTextDown' statLogo={down} stats='2%' logo={facebook} followers='52' type='Likes' text={text2} headText={headText} card={card}/>
             </div>
             <div className="mx-5">
-                <BottomRowCardsComponent statsColor='statsTextUp' statLogo={up} stats='2257%' logo={instagram} followers='5462' type='Likes'/>
+                <BottomRowCardsComponent statsColor='statsTextUp' statLogo={up} stats='2257%' logo={instagram} followers='5462' type='Likes' text={text2} headText={headText} card={card}/>
             </div>
             <div className="mx-5">
-                <BottomRowCardsComponent statsColor='statsTextUp' statLogo={up} stats='1375%' logo={instagram} followers='52k' type='Profile Views'/>
+                <BottomRowCardsComponent statsColor='statsTextUp' statLogo={up} stats='1375%' logo={instagram} followers='52k' type='Profile Views' text={text2} headText={headText} card={card}/>
             </div>
             <div className="mx-5">
-                <BottomRowCardsComponent statsColor='statsTextUp' statLogo={up} stats='303%' logo={twitter} followers='117' type='Retweets'/>
+                <BottomRowCardsComponent statsColor='statsTextUp' statLogo={up} stats='303%' logo={twitter} followers='117' type='Retweets' text={text2} headText={headText} card={card}/>
             </div>
             <div className="mx-5">
-                <BottomRowCardsComponent statsColor='statsTextUp' statLogo={up} stats='553%' logo={twitter} followers='507' type='Likes'/>
+                <BottomRowCardsComponent statsColor='statsTextUp' statLogo={up} stats='553%' logo={twitter} followers='507' type='Likes' text={text2} headText={headText} card={card}/>
             </div>
             <div className="mx-5">
-                <BottomRowCardsComponent statsColor='statsTextDown' statLogo={down} stats='19%' logo={youtube} followers='107' type='Likes'/>
+                <BottomRowCardsComponent statsColor='statsTextDown' statLogo={down} stats='19%' logo={youtube} followers='107' type='Likes' text={text2} headText={headText} card={card}/>
             </div>
             <div className="mx-5">
-                <BottomRowCardsComponent statsColor='statsTextDown' statLogo={down} stats='12%' logo={youtube} followers='1407' type='Total Views'/>
+                <BottomRowCardsComponent statsColor='statsTextDown' statLogo={down} stats='12%' logo={youtube} followers='1407' type='Total Views' text={text2} headText={headText} card={card}/>
             </div>
           </div>
         </div>
--- src/components/TopRowCardsComponent.tsx
+++ src/components/TopRowCardsComponent.tsx
@@ -4,12 +4,12 @@ import '../App.css';
 const TopRowCardsComponent = (prop:any) => {
   return (
     <div>
-      <div className='darkCard text-center' style={{borderBottomLeftRadius:'5px', borderBottomRightRadius:'5px'}}>
+      <div className={`${prop.card} text-center`} style={{borderBottomLeftRadius:'5px', borderBottomRightRadius:'5px'}}>
         <div className={`h-[6px] ${prop.topBorder}`} style={{borderTopLeftRadius:'5px', borderTopRightRadius:'5px'}}></div>
         
-        <p className='darkText2'><img className='inline me-2 my-6' src={prop.logo} alt='social media logo'/>{prop.username}</p>
-        <h1 className='darkText1 text-[50px] leading-none'>{prop.followers}</h1>
-        <p className='darkText2 tracking-[0.25em]'>{prop.type}</p>
+        <p className={prop.text}><img className='inline me-2 my-6' src={prop.logo} alt='social media logo'/>{prop.username}</p>
+        <h1 className={`${prop.headText} text-[50px] leading-none`}>{prop.followers}</h1>
+        <p className={`${prop.text} tracking-[0.25em]`}>{prop.type}</p>
         <p className={prop.statsColor}><img className='inline me-2 my-8' src={prop.statLogo} alt='statistics arrow'/>{prop.stats}</p>
       </div>
     </div>
