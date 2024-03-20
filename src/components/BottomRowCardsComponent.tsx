3afd4793ba1cbd54b4833c2019606e4526729db7
brandoughnut <brandonnguyen2867@gmail.com>
2024-03-19 23:36:04 -0700

    minor fixes

--- src/components/BottomRowCardsComponent.tsx
+++ src/components/BottomRowCardsComponent.tsx
@@ -5,7 +5,7 @@ const BottomRowCardsComponent = (prop: {card:string, text:string, type:string, l
   return (
     <div>
       <div className={`${prop.card} grid grid-cols-2 px-6 py-4`} style={{borderRadius:'5px'}}>
-        <div className={`${prop.text} interBold`}>{prop.type}</div>
+        <div className={`${prop.text} interBold whitespace-nowrap`}>{prop.type}</div>
         <div className='grid justify-end'><img src={prop.logo} alt='social media logo'/></div>
         <div className={`${prop.headText} text-[35px] leading-none mt-10`}>{prop.followers}</div>
         <div className={`text-end mt-auto leading-none ${prop.statsColor}`}><img className='inline me-2' src={prop.statLogo} alt='statistics arrow'/>{prop.stats}</div>
